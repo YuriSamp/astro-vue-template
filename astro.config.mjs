34725b14584a49a03d27ddb771878303954d90e9
YuriSamp <yurisamp123@gmail.com>
2023-05-14 02:34:23 -0300

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind()],
+  output: "server",
+  adapter: vercel()
+});
